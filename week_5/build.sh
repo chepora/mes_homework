3805f31b9da52feef566c5ca1dbac0f89efe929f
chepora <25749463+chepora@users.noreply.github.com>
2023-10-30 23:02:58 +0100

    Basic Commands running

--- week_5/build.sh
+++ week_5/build.sh
@@ -1,23 +1,19 @@
 #!/usr/bin/env bash
 
 ROOTDIR=`pwd`
-#CONFIGS="debug release"
-CONFIGS="debug" 
+CONFIGS="debug release"
 
 PICO_BOARD=adafruit_feather_rp2040
-PICO_TOOL_PATH=/home/agr/pico/picotool/build/picotool
 
 for CONFIG in $CONFIGS; do
     BUILDFOLDER=$ROOTDIR/build/$CONFIG
-    #DISTFOLDER=$ROOTDIR/dist/$CONFIG
 
-    mkdir -p $BUILDFOLDER #$DISTFOLDER    
+    mkdir -p $BUILDFOLDER  
 
     cd $BUILDFOLDER
 
     cmake -DPICO_BOARD=$PICO_BOARD -DCMAKE_BUILD_TYPE=$CONFIG $ROOTDIR || exit 1
 
     make
-    #make install
 
 done
--- week_5/src/main.c
+++ week_5/src/main.c
@@ -8,18 +8,17 @@
 
 int main() {
 
-    const uint8_t LED_PIN       = 13;
-
+    const uint8_t LED_PIN           = 13;
     const uint32_t BLINK_TIME_MS    = 1000;
 
     BLINKY_VAR used_blinky_params = {
         .led_var       = {.led_pin = LED_PIN, .blink_time_ms = BLINK_TIME_MS},
     };
 
-    blinky_init(&used_blinky_params);
-    blinky_loop(&used_blinky_params);
+    //blinky_init(&used_blinky_params);
+    //blinky_loop(&used_blinky_params);
 
-
+    stdio_init_all();
 	ConsoleInit();
 
 	while(1) 
